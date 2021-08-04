63acd2f2f1b30fa835c6a48e9b02da8bf6f0c44a
Akshay Kumar T N <akshay.t@tvsnext.io>
2021-08-04 09:19:13 +0530

    [Akshay] Added -> Connect to DataBase using ADO.Net

---
+++ EmployeePayrollService/EmployeeRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EmployeePayrollService
+{
+    class EmployeeRepo
+    {
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=payroll_service;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+    }
+}
