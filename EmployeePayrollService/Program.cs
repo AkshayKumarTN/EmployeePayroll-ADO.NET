7419765ba4e7e15ecec0c8a8a52c4025b18054f8
Akshay Kumar T N <akshay.t@tvsnext.io>
2021-08-06 16:40:19 +0530

    [Akshay] Added -> Method to Add List of Employee To DB With/Without Thread

--- EmployeePayrollService/Program.cs
+++ EmployeePayrollService/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EmployeePayrollService
 {
@@ -24,6 +25,18 @@ namespace EmployeePayrollService
 
             // Remove Employee From DataBase By Passing EmployeeID.................
             repo.RemoveEmployee(5);
+
+            ThreadOperations threadOperations = new ThreadOperations();
+            // Created a Employee List......................
+            List<EmployeeModel> listEmployee = new List<EmployeeModel>();
+            // Adding Employee to Employee List..................
+            listEmployee.Add(employee);
+
+            // Method to Add List of Contacts To DB Without Thread..................
+            threadOperations.AddEmployeeListToDBWithoutThread(listEmployee);
+            // Method to Add List of Contacts To DB With Thread..................
+            threadOperations.AddEmployeeListToDBWithThread(listEmployee);
+
         }
     }
 }
---
+++ EmployeePayrollService/ThreadOperations.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading;
+
+namespace EmployeePayrollService
+{
+    public class ThreadOperations
+    {
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=payroll_service;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        // Method to Add New Employee To DataBase .................
+        public bool AddEmployee(EmployeeModel employee)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand sqlCommand = new SqlCommand("spAddEmployee", this.connection);
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.AddWithValue("@Id", employee.EmployeeID);
+                    sqlCommand.Parameters.AddWithValue("@Name", employee.EmployeeName);
+                    sqlCommand.Parameters.AddWithValue("@PhoneNumber", employee.PhoneNumber);
+                    sqlCommand.Parameters.AddWithValue("@Address", employee.Address);
+                    sqlCommand.Parameters.AddWithValue("@Gender", employee.Gender);
+                    sqlCommand.Parameters.AddWithValue("@Department", employee.Department);
+                    sqlCommand.Parameters.AddWithValue("@Salary", employee.BasicPay);
+                    sqlCommand.Parameters.AddWithValue("@Deduction", employee.Deductions);
+                    sqlCommand.Parameters.AddWithValue("@TaxablePay", employee.TaxablePay);
+                    sqlCommand.Parameters.AddWithValue("@IncomeTax", employee.Tax);
+                    sqlCommand.Parameters.AddWithValue("@NetPay", employee.NetPay);
+                    sqlCommand.Parameters.AddWithValue("@StartDate", DateTime.Now);
+
+
+                    this.connection.Open();
+                    var result = sqlCommand.ExecuteNonQuery();
+                    this.connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+            return false;
+        }
+        // Method to Add List of Employee To DB Without Thread..................
+        public void AddEmployeeListToDBWithoutThread(List<EmployeeModel> listEmployee)
+        {
+
+            listEmployee.ForEach(employee =>
+            {
+                Console.WriteLine("Contact being added: " + employee.EmployeeID);
+                this.AddEmployee(employee);
+                Console.WriteLine("Contact added: " + employee.EmployeeID);
+            });
+        }
+
+        // Method to Add List of Employee To DB With Thread......................
+        public void AddEmployeeListToDBWithThread(List<EmployeeModel> contactList)
+        {
+            contactList.ForEach(employee =>
+            {
+                Thread thread = new Thread(() =>
+                {
+                    Console.WriteLine("Contact Being added" + employee.EmployeeID);
+                    this.AddEmployee(employee);
+                    Console.WriteLine("Contact added: " + employee.EmployeeID);
+                });
+                thread.Start();
+                thread.Join();
+            });
+        }
+    }
+}
