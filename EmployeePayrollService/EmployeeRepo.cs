24b4479842183d63b80df49a7f89369ab2023abe
Akshay Kumar T N <akshay.t@tvsnext.io>
2021-08-06 15:25:00 +0530

    [Akshay] Added -> New Employee to DataBase

--- EmployeePayrollService/EmployeeRepo.cs
+++ EmployeePayrollService/EmployeeRepo.cs
@@ -210,5 +210,49 @@ namespace EmployeePayrollService
                 this.connection.Close();
             }
         }
+
+        // Method to Add New Employee To DataBase .................
+        public bool AddEmployee(EmployeeModel employee)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand sqlCommand = new SqlCommand("spAddEmployee", this.connection);
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.AddWithValue("@Id", employee.EmployeeID);
+                    sqlCommand.Parameters.AddWithValue("@Name", employee.EmployeeName);
+                    sqlCommand.Parameters.AddWithValue("@PhoneNumber", employee.PhoneNumber);
+                    sqlCommand.Parameters.AddWithValue("@Address", employee.Address);
+                    sqlCommand.Parameters.AddWithValue("@Gender", employee.Gender);
+                    sqlCommand.Parameters.AddWithValue("@Department", employee.Department);
+                    sqlCommand.Parameters.AddWithValue("@Salary", employee.BasicPay);
+                    sqlCommand.Parameters.AddWithValue("@Deduction", employee.Deductions);
+                    sqlCommand.Parameters.AddWithValue("@TaxablePay", employee.TaxablePay);
+                    sqlCommand.Parameters.AddWithValue("@IncomeTax", employee.Tax);
+                    sqlCommand.Parameters.AddWithValue("@NetPay", employee.NetPay);
+                    sqlCommand.Parameters.AddWithValue("@StartDate", DateTime.Now);
+
+
+                    this.connection.Open();
+                    var result = sqlCommand.ExecuteNonQuery();
+                    this.connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+            return false;
+        }
     }
 }
--- EmployeePayrollService/Program.cs
+++ EmployeePayrollService/Program.cs
@@ -8,7 +8,20 @@ namespace EmployeePayrollService
         {
             Console.WriteLine("Employee Payroll Service \n");
             EmployeeRepo repo = new EmployeeRepo();
-            repo.PerformAggregateFunctions();
+            EmployeeModel employee = new EmployeeModel();
+
+            employee.EmployeeID=5;
+            employee.EmployeeName="Sara";
+            employee.PhoneNumber="9678123459";
+            employee.Address="US";
+            employee.Gender = "F";
+            employee.Department= "Marketing";
+            employee.BasicPay= 250000;
+            employee.Deductions= 18000;
+            employee.TaxablePay= 240000;
+            employee.Tax= 4000;
+            employee.NetPay= 259000;
+            repo.AddEmployee(employee);
         }
     }
 }
